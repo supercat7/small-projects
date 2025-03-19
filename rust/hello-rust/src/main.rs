9d9964e71e688e3534f98273715f8016cfb3ab39
supercat7 <superuser3344@protonmail.com>
2025-03-18 20:54:45 -0400

    added rust

---
+++ rust/hello-rust/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello world!");
+    let mut x: i32 = 50;
+    println!("I have {}", x);
+    x = 11;
+    println!("I have {}", x);
+
+}
---
+++ rust/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/superuser/Sync/docs/programming/small-projects/rust/hello-rust/target/debug/hello-rust: /home/superuser/Sync/docs/programming/small-projects/rust/hello-rust/src/main.rs
