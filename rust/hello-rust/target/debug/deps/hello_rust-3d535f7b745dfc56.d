71e39518652ee491c728958f43365107c8101451
supercat7 <superuser3344@protonmail.com>
2025-03-18 20:57:38 -0400

    added rust

--- rust/hello-rust/target/debug/deps/hello_rust-3d535f7b745dfc56.d
+++
@@ -1,5 +0,0 @@
-/home/superuser/Sync/docs/programming/small-projects/rust/hello-rust/target/debug/deps/hello_rust-3d535f7b745dfc56: src/main.rs
-
-/home/superuser/Sync/docs/programming/small-projects/rust/hello-rust/target/debug/deps/hello_rust-3d535f7b745dfc56.d: src/main.rs
-
-src/main.rs:
--- rust/hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/superuser/Sync/docs/programming/small-projects/rust/hello-rust/target/debug/hello-rust: /home/superuser/Sync/docs/programming/small-projects/rust/hello-rust/src/main.rs
